e0405a0dab746fc578168d6d7cfe08abc6b37877
Leonid_Khristyuk <kasheyne@gmail.com>
2022-10-27 21:57:18 +0300

    Exercises 2022.10.27

--- Topics/Iterating over arrays/Two numbers never occur next to each other/src/Main.java
+++ Topics/Iterating over arrays/Two numbers never occur next to each other/src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        scanner.nextLine();
+        String array = scanner.nextLine();
+        String[] strArray = array.split(" ");
+        int[] intArray = new int[size];
+        for (int i = 0; i < strArray.length; i++) {
+            intArray[i] = Integer.parseInt(strArray[i]);
+        }
+        String valuesStr = scanner.nextLine();
+        String[] valuesArrayStr = valuesStr.split(" ");
+        int n = Integer.parseInt(valuesArrayStr[0]);
+        int m = Integer.parseInt(valuesArrayStr[1]);
+
+        boolean result = true;
+        for (int i = 0; i < intArray.length; i++) {
+            if (i == 0) {
+                if (intArray[i] == n && intArray[i + 1] == m) {
+                    result = false;
+                }
+            } else if (i == intArray.length - 1) {
+                if (intArray[i] == n && intArray[i - 1] == m) {
+                    result = false;
+                }
+            } else {
+                if (intArray[i] == n && (intArray[i + 1] == m || intArray[i - 1] == m)) {
+                    result = false;
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
