b86a00ede4e09deff7347281770e89bdc0ebb35f
Leonid_Khristyuk <kasheyne@gmail.com>
2022-12-04 22:36:06 +0300

    Exercises 2022.12.04

---
+++ Topics/The Collections Framework overview/Blacklist/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class CollectionUtils {
+
+    public static Collection<String> filterPhones(Collection<String> phones,
+                                                  Collection<String> blacklist) {
+        Iterator<String> iterator = phones.iterator();
+        while (iterator.hasNext()) {
+            String tmp = iterator.next();
+            if (blacklist.contains(tmp)) {
+                iterator.remove();
+            }
+        }
+
+        return phones;
+    }
+}
+
+/* Please, do not modify this I/O code */
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Collection<String> phones = Arrays.asList(scanner.nextLine().split("\\s+"));
+        Collection<String> blockList = Arrays.asList(scanner.nextLine().split("\\s+"));
+
+        Collection<String> nonBlockedPhones = CollectionUtils.filterPhones(
+                new ArrayList<>(phones), new ArrayList<>(blockList));
+
+        System.out.println(nonBlockedPhones.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(" ")));
+    }
+}
